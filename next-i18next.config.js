deca960975e092abc201315a2fae078b0b15924a
Jorge Moreira <jorge.moreira@weareplanet.com>
2025-10-26 22:06:28 +0000

    fix: try fix of locales load on vercel

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  i18n: {
-    defaultLocale: "pt",
-    locales: ["en", "pt"],
-  },
+import { resolve } from 'path';
+
+export const i18n = {
+  defaultLocale: "pt",
+  locales: ["en", "pt"],
 };
+export const localePath = resolve('./public/locales');
